f2e7765b5685d81049eaaf1b8b6c76fefdfd171c
Leo Cassarani <leo.cassarani@me.com>
2013-02-09 15:20:43 +0000

    Move Gitlactica::Application class into lib/gitlactica.rb

--- lib/gitlactica.rb
+++ lib/gitlactica.rb
@@ -3,12 +3,44 @@ require 'em-http-request'
 require 'em-websocket'
 require 'yajl'
 
-require_relative 'gitlactica/application'
 require_relative 'gitlactica/client'
 require_relative 'gitlactica/message'
-
 require_relative 'gitlactica/github'
 
 module Gitlactica
+  class Application
+    def self.run
+      new.run
+    end
 
+    def initialize
+      @clients = []
+    end
+
+    def run
+      EM::WebSocket.start(host: 'localhost', port: 8080) do |ws|
+        client = Client.new(ws)
+        ws.onopen  { client_add(client) }
+        ws.onclose { client_remove(client) }
+        ws.onmessage { |msg| client_msg(client, msg) }
+      end
+    end
+
+    private
+
+    def client_add(client)
+      @clients << client
+    end
+
+    def client_remove(client)
+      @clients.delete(client)
+    end
+
+    def client_msg(client, json)
+      msg = Message.new(json)
+      client.process(msg)
+    rescue Message::InvalidJSONError, Client::InvalidMessageError
+      puts "Invalid message received: #{json}"
+    end
+  end
 end
