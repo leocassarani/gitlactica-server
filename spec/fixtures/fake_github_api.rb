da5ff937fcd7d5e1bf20a27ee242c891bef708af
Leo Cassarani <leo.cassarani@me.com>
2013-02-19 00:23:55 +0000

    Mimic the GitHub API when responding with a 404

--- spec/fixtures/fake_github_api.rb
+++ spec/fixtures/fake_github_api.rb
@@ -1,20 +1,27 @@
 class FakeGitHubApi < Sinatra::Base
   GITHUB_API_DIR = File.join(File.dirname(__FILE__), 'github_api')
 
-  get '/users/defunkt/repos' do
+  include JSONHelper
+
+  before do
     content_type :json
+  end
+
+  get '/users/defunkt/repos' do
     respond_with_fixture('defunkt_repos.json')
   end
 
   get '/repos/garybernhardt/raptor/commits' do
-    content_type :json
     respond_with_fixture('raptor_commits.json')
   end
 
   get '/repos/garybernhardt/raptor/git/trees/master' do
     pass unless params.has_key?('recursive')
-    content_type :json
     respond_with_fixture('raptor_tree.json')
+  end
+
+  not_found do
+    to_json(message: "Not Found")
   end
 
   private
--- spec/integration/integration_helper.rb
+++ spec/integration/integration_helper.rb
@@ -9,8 +9,8 @@ require 'yajl'
 
 require './lib/gitlactica'
 
-require './spec/fixtures/fake_github_api'
 require './spec/support/eventmachine_helper'
 require './spec/support/github_api_helper'
 require './spec/support/json_helper'
 require './spec/support/web_socket_helper'
+require './spec/fixtures/fake_github_api'
