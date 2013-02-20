f86841bc6b6e1bd2d0e2d4caaa2d32fc0c5a2d76
Leo Cassarani <leo.cassarani@me.com>
2013-02-20 00:30:19 +0000

    Slightly improve tests for the RepoComplexity module

--- spec/gitlactica/repo_complexity_spec.rb
+++ spec/gitlactica/repo_complexity_spec.rb
@@ -12,18 +12,25 @@ module Gitlactica
     let(:interesting) { mock(:interesting, programming?: true) }
     let(:uninteresting) { mock(:uninteresting, programming?: false) }
 
-    before { Language.stub(:for_extension).with('.rb') { interesting } }
-    before { Language.stub(:for_extension).with('.js') { interesting } }
-    before { Language.stub(:for_extension).with('.txt') { uninteresting } }
+    before do
+      Language.stub(:for_extension).with('.rb') { interesting }
+      Language.stub(:for_extension).with('.js') { interesting }
+      Language.stub(:for_extension).with('.txt') { uninteresting }
+    end
 
     let(:tree) { mock(:tree) }
 
     it "returns the average file size of all interesting blobs" do
-      tree.stub(:blobs) { [ruby, txt, js] }
+      tree.stub(:blobs) { [ruby, js] }
       Gitlactica::RepoComplexity.for_tree(tree).should == 150
     end
 
-    it "returns 0 when given no interesting blobs" do
+    it "ignores uninteresting blobs" do
+      tree.stub(:blobs) { [ruby, txt] }
+      Gitlactica::RepoComplexity.for_tree(tree).should == 100
+    end
+
+    it "returns 0 when there are no interesting blobs" do
       tree.stub(:blobs) { [txt] }
       Gitlactica::RepoComplexity.for_tree(tree).should == 0
     end
